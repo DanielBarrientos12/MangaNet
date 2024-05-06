e0747f51f23b016c3bc4c1f72f19ce0d1b798d7c
DanielBarrientos12 <danielbarrientos15342@outlook.com>
2024-05-06 15:14:33 -0500

    repository started

---
+++ src/main/java/com/manganet/MangaNetApplication.java
@@ -0,0 +1,13 @@
+package com.manganet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manganet/MangaNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manganet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
